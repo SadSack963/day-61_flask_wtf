03ffee3de7fc3ea0fd37887f464cab3755727cfc
john <john@jpatmore.plus.com>
2021-03-28 04:27:17 +0100

    Start of Flask-Bootstrap

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from my_form import LoginForm
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "my secret key"  # Use .env
 
 
@@ -14,18 +16,23 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and \
+                login_form.password.data == '12345678':
+            return redirect(url_for('success'))
+        else:
+            return redirect(url_for('denied'))
     return render_template('login.html', form=login_form)
 
 
-# @app.route("/success")
-# def success():
-#     return render_template('success.html')
-#
-#
-# @app.route("/denied")
-# def success():
-#     return render_template('denied.html')
+@app.route("/success")
+def success():
+    return render_template('success.html')
+
+
+@app.route("/denied")
+def denied():
+    return render_template('denied.html')
 
 
 if __name__ == '__main__':
